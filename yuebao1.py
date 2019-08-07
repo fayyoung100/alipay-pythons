199aa53d2b2ff8b52202b3db54b7cfc3246d11f7
YolaMa <2820199837@qq.com>
2019-08-07 20:06:09 +0800

    体验金小米手机数据点

--- yuebao1.py
+++ yuebao1.py
@@ -3,11 +3,11 @@ from com.android.monkeyrunner import MonkeyRunner,MonkeyDevice,MonkeyImage
 device=MonkeyRunner.waitForConnection()
 
 
-for i in range(1,1000):
+for i in range(1,1100):
   print "time : %s" % i
-  time.sleep(1.5)
-  device.touch(1225,2153,'DOWN_AND_UP')
+  time.sleep(2.5)
+  device.touch(600,1117,'DOWN_AND_UP')
   print "user money"
-  time.sleep(0.5)
-  device.touch(328,2787,'DOWN_AND_UP')
+  time.sleep(1.5)
+  device.press('KEYCODE_BACK','DOWN_AND_UP')
 print "user complete"
